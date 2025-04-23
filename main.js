4f07d4b3034ff5bebd141b6d3806b22c29be8d17
Leofront-end <ls171258@gmail.com>
2025-04-22 21:36:02 -0300

    iniciando a parte 3 do video

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const characterId = document.getElementById('characterId')
 const btnGo = document.getElementById('btn-go')
+const btnReset = document.getElementById('btn-reset')
 const content = document.getElementById('content')
+const containerResult = document.getElementById('result-style')
 const image = document.getElementById('img')
 
 const fetchApi = (value) => {
@@ -14,11 +16,24 @@ const fetchApi = (value) => {
     return result
 }
 
+const keys = ['name', 'status', 'species', 'gender', 'origin', 'episode']
+
+const buildResult = (result) => {
+    const newObject = {}
+    keys.map((key) => document.getElementById(key))
+        .map((elem) => {
+            elem.checked && (newObject[elem.name] = result[elem.name])
+        })
+
+
+    return newObject
+}
 
 btnGo.addEventListener('click',async (event) => {
     event.preventDefault()
     const result = await fetchApi(characterId.value)
-    content.textContent = `${JSON.stringify(result, undefined, 2)}`
+    // content.textContent = `${JSON.stringify(result, undefined, 2)}`
+    content.textContent = `${JSON.stringify(buildResult(result), undefined, 2)}`
     image.src = `${result.image}`
-    image.alt = `imagem do ${result.name}`
+    console.log(buildResult(result))
 })
